6ef5756d559f15543776f89f14037fffaf4af9fb
AliceGem <alice.geminiani@unipv.it>
2023-03-23 09:54:49 +0000

    Conflict merge fixing

--- read_online_tm_signals.py
+++ read_online_tm_signals.py
@@ -6,7 +6,7 @@ Created on Tue Feb  7 16:59:15 2023
 import os
 paw_otrack = 'FR'
 path_main = 'C:\\Users\\alice\\Dropbox (Personal)\\CareyLab\\materialAnaG\\threshold_test_20032023\\'#'C:\\Users\\Ana\\Documents\\PhD\\Projects\\Online Stimulation Treadmill\\tests 200323\\thresholds\\'
-subdir = ''   #'hrpaw\\'
+subdir = 'hr\\'   #'hrpaw\\'
 path = os.path.join(path_main, subdir)
 main_dir = path.split('\\')[:-2]
 animal = 'MC16946HR'
@@ -23,6 +23,7 @@ trials = otrack_class.get_trials()
 # READ SYNCHRONIZER SIGNALS
 [timestamps_session, frame_counter_session, trial_signal_session, sync_signal_session, laser_signal_session, laser_trial_signal_session] = otrack_class.get_synchronizer_data(camera_frames_kept, plot_data)
 
+
 # READ ONLINE DLC TRACKS
 otracks = otrack_class.get_otrack_excursion_data(timestamps_session)
 [otracks_st, otracks_sw] = otrack_class.get_otrack_event_data(timestamps_session)
@@ -36,13 +37,3 @@ otracks = otrack_class.get_otrack_excursion_data(timestamps_session)
 # READ MP4 AND OVERLAY OFFLINE AND ONLINE DLC TRACKS
 for t in trials:
     otrack_class.overlay_tracks_video(t, paw_otrack, offtracks_st, offtracks_sw, otracks_st, otracks_sw)
-
-# READ OFFLINE DLC TRACKS
-[offtracks_st, offtracks_sw] = otrack_class.get_offtrack_event_data(paw_otrack, loco, animal, session)
-
-# LATENCY OF LIGHT IN RELATION TO OTRACK
-[latency_light_st, latency_light_sw, st_led_on, sw_led_on] = otrack_class.get_led_information_trials(trials, timestamps_session, otracks_st, otracks_sw)
-
-# READ MP4 AND OVERLAY OFFLINE AND ONLINE DLC TRACKS
-#for t in trials:
-#     otrack_class.overlay_tracks_video(t, paw_otrack, offtracks_st, offtracks_sw, otracks_st, otracks_sw)
