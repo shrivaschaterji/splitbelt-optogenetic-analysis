6166d0c766490b4780a83e9e125f230d2a5fb00b
anagigoncalves <ana.gigoncalves@gmail.com>
2023-04-02 22:10:25 +0100

    crop mp4 file

---
+++ crop_mp4.py
@@ -0,0 +1,18 @@
+from decord import VideoReader
+from decord import cpu
+import cv2
+import glob
+
+path = 'C:\\Users\\Ana\\Documents\\PhD\\DLC projects\\videos for cropped network\\'
+videos = glob.glob(path + '*.mp4')
+sr = 333
+frame_width = 1088
+frame_height = 140
+for filename in videos:
+    out = cv2.VideoWriter(filename[:-4] + '_crop.mp4', cv2.VideoWriter_fourcc(*'XVID'), sr, (frame_width, frame_height), True)
+    vidObj = VideoReader(filename, ctx=cpu(0))  # read the video
+    frames_total = len(vidObj)
+    for frameNr in range(frames_total):
+        frame = vidObj[frameNr]
+        frame_np = frame.asnumpy()
+        out.write(frame_np[280:, :, :])
