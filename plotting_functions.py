45ec4a3665b8a52888c37cf40c82e5527ef033a5
“AliceGem“ <alice.geminiani@polimi.it>
2025-01-21 17:06:36 +0000

    Improved ind animals plot function

--- plotting_functions.py
+++ plotting_functions.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Learning curves
 # Individual animals
-def plot_learning_curve_ind_animals(param_sym, current_param, param_labels, animals, colors, split_intervals=None, stim_intervals=None):
+def plot_learning_curve_ind_animals(param_sym, current_param, param_labels, animal_list, colors, intervals=None):
     """
     Plots the learning curve for individual animals, each one with the animal's color.
     Overlaps the start and end of split/stimulation intervals, if specified.
@@ -16,38 +16,35 @@ def plot_learning_curve_ind_animals(param_sym, current_param, param_labels, anim
     Parameters:
     param_sym (numpy.ndarray): A 3D array containing the parameter values for each parameter, animal and trial.
     current_param (int): The index of the current parameter to plot.
-    param_labels (list of str): A list of labels for each parameter.
-    animals (list of str): A list of animal identifiers.
+    param_labels (dict): Dictionary keys are the parameter names and values are the corresponding labels.
+    animal_list (list of str): A list of animal identifiers.
     colors (dict): A dictionary mapping animal identifiers to colors.
-    split_intervals (list, optional): A list containing the start and duration (in trials) of the split interval.
-    stim_intervals (list, optional): A list containing the start and duration (in trials) of the stimulation interval.
-    
+    intervals (dict, optional): A dictionary containing 'split' and 'stim' intervals with their respective start and duration (in trials).
+
     Returns:
     fig (matplotlib.figure.Figure): The figure object containing the plot.
     """
     fig, ax = plt.subplots(figsize=(7, 10), tight_layout=True)
-    
-    if split_intervals is not None:
-        split_start, split_duration = split_intervals
-        rectangle = plt.Rectangle((split_start - 0.5, np.nanmin(param_sym[current_param, :, :].flatten())), split_duration,
-                                    np.nanmax(param_sym[current_param, :, :].flatten()) - np.nanmin(param_sym[current_param, :, :].flatten()),
-                                    fc='lightblue', alpha=0.3)
-        plt.gca().add_patch(rectangle)
-    
-    if stim_intervals is not None:
-        stim_start, stim_duration = stim_intervals
-        ax.axvline(x=stim_start-0.5, color='k', linestyle='-', linewidth=0.5)
-        ax.axvline(x=stim_start+stim_duration-0.5, color='k', linestyle='-', linewidth=0.5)
-    
+
+    # Plot learning curves for each animal
+    for a in range(np.shape(param_sym)[1]):  # Loop on all animals
+        plt.plot(np.linspace(1, len(param_sym[current_param, a, :]), len(param_sym[current_param, a, :])), param_sym[current_param, a, :], color=colors[animal_list[a]],
+                 label=animal_list[a], linewidth=2)
+         
+    # Add split and stimulation intervals
+    if intervals:
+        if 'split' in intervals:
+            add_patch_interval(ax, intervals['split'])
+        if 'stim' in intervals:
+            add_start_end_interval(ax, intervals['stim'])
+
+    # Add horizontal line at 0    
     plt.hlines(0, 1, len(param_sym[current_param, 0, :]), colors='grey', linestyles='--')
     
-    for a in range(np.shape(param_sym)[1]):  # Loop on all animals
-        plt.plot(np.linspace(1, len(param_sym[current_param, a, :]), len(param_sym[current_param, a, :])), param_sym[current_param, a, :], color=colors[animals[a]],
-                 label=animals[a], linewidth=2)
-    
+    # Add figure labels and legend
     ax.set_xlabel('Trial', fontsize=24)
     ax.legend(frameon=False, loc='center left', bbox_to_anchor=(1, 0.5))
-    ax.set_ylabel(param_labels[current_param] + ' asymmetry', fontsize=24)
+    ax.set_ylabel(list(param_labels.values())[current_param] + ' asymmetry', fontsize=24)
     
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
@@ -56,7 +53,7 @@ def plot_learning_curve_ind_animals(param_sym, current_param, param_labels, anim
 
     return fig 
 
-
+ 
 
 # Individual animals with average
 def plot_learning_curve_ind_animals_avg(param_sym_avg, current_param, param_labels, animal_list, included_animals, colors, intervals=None, experiment_name=None, ranges=[False, None]):
